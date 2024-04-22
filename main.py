8dc41f10b6704e542e537af7cf27cff5051d5c84
faspeice <dima_kolodin@bk.ru>
2024-04-22 20:21:34 +0600

    poetry email

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from pydantic import BaseModel, EmailStr
-
+from typing import Annotated
+from items_views import router as items_router
+from users.views import router as users_router
 import uvicorn
 
 app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
 
 
-class CreateUser(BaseModel):
-    email: EmailStr
+
 
 
 @app.get("/")
@@ -23,12 +26,7 @@ def hello(name: str = "World"):
     return {"message": f"Hello {name}!"}
 
 
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return {
-        "message": "success",
-        "email": user.email,
-    }
+
 
 
 @app.get("/calc/add/")
@@ -40,27 +38,6 @@ def add(a: int, b: int):
     }
 
 
-@app.get("/items/")
-def list_items():
-    return [
-        "Item1",
-        "Item2",
-        "Item3",
-    ]
-
-
-@app.get("/items/latest/")
-def get_latest_item():
-    return {"item": {"id": "0", "name": "latest"}}
-
-
-@app.get("/items/{item_id}/")
-def get_item_by_id(item_id: int):
-    return {
-        "item": {
-            "id": item_id,
-        },
-    }
 
 
 if __name__ == '__main__':
