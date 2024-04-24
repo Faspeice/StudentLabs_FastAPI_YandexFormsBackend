69338dc2992dbeecc065aa470683ab7050cd96e9
faspeice <dima_kolodin@bk.ru>
2024-04-24 16:00:37 +0600

    Create relationships between User - Forms

--- core/models/form.py
+++ core/models/form.py
@@ -1,11 +1,14 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
 from .base import Base
 
+if TYPE_CHECKING
+    from .user import User
 
 class Form(Base):
     __tablename__ = "forms"
     type: Mapped[str]
     description: Mapped[str]
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] =relationship(back_populates="forms")
--- core/models/user.py
+++ core/models/user.py
@@ -1,7 +1,10 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 from .base import Base
 from sqlalchemy import String
-
+from typing import TYPE_CHECKING
+if TYPE_CHECKING
+    from .form import Form
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
+    forms = Mapped[list["Form"]] = relationship(back_populates="user")
