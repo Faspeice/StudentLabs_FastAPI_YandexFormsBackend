1014b6de924f0473e7c238d12b7945e9801c0f8d
faspeice <dima_kolodin@bk.ru>
2024-04-24 15:54:37 +0600

    migr Form

--- core/models/form.py
+++ core/models/form.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
@@ -7,3 +8,4 @@ class Form(Base):
     __tablename__ = "forms"
     type: Mapped[str]
     description: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
